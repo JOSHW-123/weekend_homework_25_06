04e7c273ae864cc0adbe610a7750b92b1e0649a8
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-06-26 12:07:05 +0100

    First test working

--- codeclan_caraoke/classes/guest.py
+++ codeclan_caraoke/classes/guest.py
@@ -1,14 +1,23 @@
 class Guest():
 
-    def __init__(self, name, wallet, status):
+    def __init__(self, name, wallet, guest_status):
         self.name = name
         self.wallet = wallet
-        self.status = status
+        self.guest_status = guest_status
 # status will show check in status.
-    
+
+    def guest_check_in_status(self, guest_status):
+        if guest_status == "Checked in":
+           return True
+        elif guest_status == "Not checked in":
+            return False
+
+        
     def check_in(self, room, name):
         pass
 
 
     def check_out(self, room):
        pass 
+
+  
--- codeclan_caraoke/tests/guest_test.py
+++ codeclan_caraoke/tests/guest_test.py
@@ -5,5 +5,10 @@ from classes.room import Room
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest = Guest("Jim", 100.00)
+        self.guest = Guest("Jim", 100, "Checked in")
+
+    def test_check_in_status(self):
+        self.assertEqual("Checked in", self.guest.guest_status)
+
+
 
